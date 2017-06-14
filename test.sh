602f1ba44ba2ead5fa9e432d98b09fdbf90c9b64
Rockford Wei <rocky@perfect.org>
2017-06-13 23:06:53 -0400

    Improving testing shell commands.

--- test.sh
+++ test.sh
@@ -1,14 +1,26 @@
 # This script is for CI Server
+echo 'Clean Temp Files'
 rm -rf /tmp/testdata
+echo 'Unzip test pack'
 tar xzf testpack.tgz -C /tmp/
-wget -q https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.1.0.tar.gz -O /tmp/testdata/linux.lib.tgz
-mkdir /tmp/testdata/linux
-tar xzf /tmp/testdata/linux.lib.tgz -C /tmp/testdata/linux
-wget -q https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-darwin-x86_64-1.1.0.tar.gz -O /tmp/testdata/darwin.lib.tgz
-mkdir /tmp/testdata/darwin
-tar xzf /tmp/testdata/darwin.lib.tgz -C /tmp/testdata/darwin
-wget -q https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip -O /tmp/testdata/in.zip
+if  [[ $OSTYPE =~ darwin* ]]; then
+	echo 'get darwin library'
+	curl https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-darwin-x86_64-1.1.0.tar.gz -o /tmp/testdata/darwin.lib.tgz
+	mkdir /tmp/testdata/darwin
+	echo 'expand darwin library'
+	tar xzf /tmp/testdata/darwin.lib.tgz -C /tmp/testdata/darwin
+else
+	echo 'get linux library'
+	curl https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.1.0.tar.gz -o /tmp/testdata/linux.lib.tgz
+	mkdir /tmp/testdata/linux
+	echo 'expand linux library'
+	tar xzf /tmp/testdata/linux.lib.tgz -C /tmp/testdata/linux
+fi
+echo 'download AI model'
+curl https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip -o /tmp/testdata/in.zip
+echo 'unzip model file'
 unzip /tmp/testdata/in.zip -d /tmp/testdata/
+echo 'testing ... '
 swift test > test-results.txt
 # swift test
 # rm -rf /tmp/testdata
